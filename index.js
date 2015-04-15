24df1c5d3173b86344eab95c68da6b9465fbd699
Rob Farmer <Rob@Robs-MacBook-Pro-5.local>
2015-04-14 20:36:20 -0400

    /echo working

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
 app.post('/echo', function(request, response) {
-  response.send(request);
+  response.send(request.body);
 });
 
 app.listen(app.get('port'), function() {
